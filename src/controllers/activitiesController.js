4dadaa66fb67ad8ab1edee771b623ab3b1001160
David Moreno <davidan7845@gmail.com>
2023-12-15 14:56:38 -0500

    getsemillerobyid

--- src/controllers/activitiesController.js
+++ src/controllers/activitiesController.js
@@ -127,6 +127,148 @@ async function enrolledActivities(req, res) {
 	}
 }
 
+// OBTENER ACTIVIDADES INSCRITAS DE UN USUARIO
+async function enrolledActivities(req, res) {
+	try {
+		const { user_id } = req.body;
+
+		console.log("UserID: " + user_id);
+
+		//OBTENER ACTIVIDADES INSCRITAS
+		const { data: dataList, error: errorList } = await supabase
+			.from("enrollments")
+			.select("*")
+			.eq("user_id", user_id);
+
+		console.log("List: " + dataList);
+
+		const activities = [];
+
+		//OBTENER DATOS ADICIONALES DE CADA ACTIVIDAD INSCRITA
+		for (const enrollment of dataList) {
+			if (enrollment.activity_type == "group" && enrollment.group_id) {
+				const { data: groupData, error: groupError } = await supabase
+					.from("groups")
+					.select("*")
+					.eq("group_id", enrollment.group_id)
+					.single();
+
+				if (groupData) {
+					activities.push({
+						group_id: enrollment.group_id,
+						group_name: groupData.group_name,
+						group_description: groupData.group_description,
+						group_photo: groupData.photo,
+					});
+				}
+			} else if (enrollment.activity_type == "event" && enrollment.event_id) {
+				const { data: eventData, error: eventError } = await supabase
+					.from("events")
+					.select("*")
+					.eq("event_id", enrollment.event_id)
+					.single();
+
+				if (eventData) {
+					activities.push({
+						event_id: enrollment.event_id,
+						event_name: eventData.event_name,
+						event_description: eventData.event_description,
+						event_photo: eventData.photo,
+					});
+				}
+			}
+		}
+
+		console.log("Activities: " + activities);
+
+		res.status(200).json({ message: "Activities sent", activities: activities });
+	} catch (error) {
+		res.status(500).json({ error: `${error}` });
+		console.log("Error: " + error);
+	}
+}
+
+async function getSemilleroById(req, res) {
+    try {
+        const { semillero_id, user_id } = req.body;
+
+        // Buscar si el semillero existe
+        const { data: semilleroData, error: semilleroError } = await supabase
+            .from("groups")
+            .select("*")
+            .eq("group_id", semillero_id)
+            .single();
+
+        // Si el semillero no existe
+        if (semilleroError) {
+            throw new Error("Group does not exist");
+        }
+
+        // Obtener la información del semillero
+        const {
+            group_name,
+            group_description,
+            slots,
+            available_slots,
+            monday_start,
+            monday_end,
+            tuesday_start,
+            tuesday_end,
+            wednesday_start,
+            wednesday_end,
+            thursday_start,
+            thursday_end,
+            friday_start,
+            friday_end,
+            saturday_start,
+            saturday_end,
+            photo,
+            place,
+        } = semilleroData;
+
+        // Verificar si el usuario está inscrito en el semillero
+        const { data: enrollmentData, error: enrollmentError } = await supabase
+            .from("enrollments")
+            .select("*")
+            .eq("user_id", user_id)
+            .eq("group_id", semillero_id)
+            .eq("activity_type", "group")
+            .single();
+
+        // El usuario está inscrito si no hay error y hay datos en la respuesta
+        const isUserEnrolled = !enrollmentError && enrollmentData;
+
+		console.log(isUserEnrolled)
+
+        // Enviar la respuesta con la información del semillero y si el usuario está inscrito
+        res.status(200).json({
+            group_name,
+            group_description,
+            slots,
+            available_slots,
+            monday_start,
+            monday_end,
+            tuesday_start,
+            tuesday_end,
+            wednesday_start,
+            wednesday_end,
+            thursday_start,
+            thursday_end,
+            friday_start,
+            friday_end,
+            saturday_start,
+            saturday_end,
+            photo,
+            place,
+            isUserEnrolled,
+        });
+    } catch (error) {
+        console.error("Error: " + `${error}`);
+        res.status(500).json({ error: `${error}` });
+    }
+}
+
+
 module.exports = {
 	makeEnrollment,
 	enrolledActivities,
