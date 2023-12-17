6769641743e2ca3bf8621dbe951277ae9bcfeb45
David Moreno <davidan7845@gmail.com>
2023-12-17 15:03:20 -0500

    cambios

--- src/routes/activitiesRoutes.js
+++ src/routes/activitiesRoutes.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const { makeEnrollment, enrolledActivities, getSemilleroById } = require("../controllers/activitiesController");
-
-router.post("/activity/enroll", makeEnrollment);
-router.post("/activity/list", enrolledActivities);
-router.get("/activity/semillero", getSemilleroById)
-=======
 
 const {
 	makeEnrollment,
 	enrolledActivities,
 	createNewActivity,
 	getEvents,
+	getSemilleroById,
     getActivities
 } = require("../controllers/activitiesController");
 
 router.post("/activity/enroll", makeEnrollment);
 router.post("/activity/list", enrolledActivities);
+router.get("/activity/semillero", getSemilleroById)
 router.post("/createnewactivity", createNewActivity);
 router.get("/events", getEvents);
 router.get("/activities", getActivities);
->>>>>>> 102566e015b2d0a5423793ab3286344c06915b9e
 
 module.exports = router;
